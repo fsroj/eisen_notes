f445dd944b9b2ab2b54d5ecaf5073e505ced33be
Reinaldo <fariasreise@gmail.com>
2025-07-10 21:51:03 -0400

    Revert "Apertura inicial de ventanas arreglada"
    This reverts commit ce533bf1ec04ff787f7b34153df24b82581200a9.

--- notes_app/app_con_calendario.py
+++ notes_app/app_con_calendario.py
@@ -1024,6 +1024,7 @@ class MainApplication:
 
         # Abrir la ventana de notas al inicio
         self.open_notes_window()
+        self.open_calendar_window() # Opcional: Abrir también el calendario al inicio
 
     def open_notes_window(self):
         notes_root = Toplevel(self.master)
@@ -1035,7 +1036,6 @@ class MainApplication:
             self.notes_app_instance.calendar_app_instance = self.calendar_app_instance
 
         notes_root.protocol("WM_DELETE_WINDOW", self._on_notes_window_close)
-        notes_root.state('zoomed')  # Maximiza la ventana (Windows)
 
 
     def open_calendar_window(self):
